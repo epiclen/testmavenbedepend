7cd192b59d99aa27ac1b7b7f4ce16e8c0112576a
liangzhao <liangzhao@alauda.io>
2019-12-19 14:05:00 +0800

    m

---
+++ src/main/java/io/test/TestClass.java
@@ -0,0 +1,14 @@
+package io.test;
+
+public class TestClass {
+    private int intval;
+    private String name;
+
+    public int getTestClassIntval() {
+        return this.intval;
+    }
+
+    public String whoAmI() {
+        return this.name;
+    }
+}
